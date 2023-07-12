5fbea487ff669adee86ca370873f217df3a1ff9d
SamuelHill27 <samuelshill04@gmail.com>
2023-07-12 02:41:28 +0100

    Format viewCount and releaseDate (momentjs)

--- src/components/Video/VideoList/Video.js
+++ src/components/Video/VideoList/Video.js
@@ -1,12 +1,32 @@
 import styles from "./Video.module.css";
 import Modal from "./Modal";
 import { useState } from "react";
+import moment from "moment/moment";
 
 const Video = ({ video, onChannelSelect }) => {
-  const date = new Date(video.releaseDate).toLocaleDateString("en-GB");
   const link = `https://www.youtube.com/embed/${video.videoId}`;
 
   const [openModel, setOpenModal] = useState(false);
+
+  const convertViews = (views) => {
+    let roundedViews = "0";
+    if (views >= 10000000) {
+      roundedViews = (views / 1000000).toFixed(0) + "M";
+    } else if (views >= 1000000) {
+      roundedViews = (views / 1000000).toFixed(1) + "M";
+    } else if (views >= 10000) {
+      roundedViews = (views / 1000).toFixed(0) + "K";
+    } else if (views >= 1000) {
+      roundedViews = (views / 1000).toFixed(1) + "K";
+    } else {
+      roundedViews = views;
+    }
+    return roundedViews;
+  };
+
+  const convertDate = (releaseDate) => {
+    return moment(new Date(releaseDate)).fromNow();
+  };
 
   const openModalHandler = () => {
     setOpenModal(true);
@@ -37,7 +57,9 @@ const Video = ({ video, onChannelSelect }) => {
       <div className={styles.video__context}>
         <div className={styles.video__title}>{video.videoTitle}</div>
         <button onClick={channelSelectHandler}>{video.channelTitle}</button>
-        <div className={styles.video__details}>{`${video.viewCount} | ${date}`}</div>
+        <div className={styles.video__details}>{`${convertViews(
+          video.viewCount
+        )} | ${convertDate(video.releaseDate)}`}</div>
       </div>
 
       {openModel && (
